86187c9f398d83243a1e600c68b31bc9ce5153c2
Shahriar Munir <smunir07@gmail.com>
2023-11-30 13:11:58 +0100

    sup-ab v1.6

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,115 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="row my-5 pt-5 z-depth-1">
+      <section>
+        <div className="row col-sm-8 col-md-12">
+          <div
+            className="view overlay rounded z-depth-2 mt-3 mb-5 clo-sm-8 col-md-12 
+            d-flex justify-content-center align-items-center"
+            style={{
+              width: "100%",
+              height: "auto",
+              maxHeight: "500px",
+              overflow: "hidden",
+              objectFit: "cover",
+            }}
+          >
+            <img
+              src="https://neuro.se/media/13372/computer-4795762_1280.jpg?center=0.57062146892655363,0.44736842105263158&mode=crop&width=830&height=440&rnd=132816084000000000"
+              className="img-fluid "
+              alt="Sample project image"
+            />
+          </div>
+        </div>
+        <div className="row col-sm-8 col-md-12">
+          <div
+            className="col-sm-8 col-md-12  d-flex flex-column justify-content-center align-items-center"
+            style={{
+              color: "#fff",
+              textAlign: "center",
+            }}
+          >
+            <h3 className="mt-4 mb-4">
+              Welcome to DevBnB: Where Developers Feels Like Home
+            </h3>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Are you a developer looking for a comfortable and inspiring space
+              to code, collaborate, and connect with like-minded individuals?
+              Look no further than DevBnb - your ultimate Airbnb for developers!
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Are you a developer looking for a comfortable and inspiring space
+              to code, collaborate, and connect with like-minded individuals?
+              Look no further than DevBnb - your ultimate Airbnb for developers!
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              At DevBnb, we understand the unique needs of developers. Whether
+              you are a seasoned pro or just starting your coding journey, our
+              platform offers a curated selection of developer-friendly
+              accommodation around the world.
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              From cozy coding corners to fully-equipped tech havens, each
+              listing is vetted to ensure it meets the standards of the coding
+              community.
+            </p>
+            <h3 className="mt-4 mb-4">Why DevBnB?</h3>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Developer-Focused Amenities: Enjoy high-speed internet, ergonomic
+              workstation, and coding-friendly environment tailored to boost
+              your productivity.
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Community Networking: Connect with fellow developers, attend
+              coding meetups, and collaborate on exciting projects, all within
+              the comfort of your DevBnb space.
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Global Reach: Explore coding-friendly accommodations in tech hubs
+              worldwide, allowing you to work and travel seamlessly.
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Secure and Trusted: Rest easy knowing that your stay is secure,
+              with reliable hosts and verified listings designed specifically
+              for developers.
+            </p>
+            <p
+              className="mx-5"
+              style={{ color: "#ebe3e385", padding: "0 9rem" }}
+            >
+              Make DevBnb your home away from home and elevate your coding
+              experience. Book your stay today and unlock a world of
+              opportunities for learning, collaboration, and innovation.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Support.jsx
+++ client/src/pages/Support.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { useUser } from "./../context/UseContext";
 
 const Support = () => {
-  return <div>Support</div>;
+  const user = useUser();
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  // function to handle the change in the input fields
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "email") {
+      setEmail(value);
+    } else {
+      setMessage(value);
+    }
+  };
+
+  return (
+    <div className="my-5" style={{ background: "black", borderRadius: "1rem" }}>
+      <section className=" dark-grey-text mb-5">
+        <div
+          className="card"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(108,108,108,1) 50%, rgba(0,0,0,1) 100%)",
+            borderRadius: "1rem",
+            color: "white",
+          }}
+        >
+          <div className="card-body p-5">
+            <h3 className=" text-center font-weight-bold my-4">Contact Us</h3>
+            <form
+              className="mb-4 mx-md-5"
+              // onSubmit={handleSubmit}
+            >
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    value={email}
+                    onChange={handleChange}
+                    // placeholder="Email"
+                    // onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="password">Message</label>
+                  <textarea
+                    type="text"
+                    id="message"
+                    className="form-control"
+                    value={message}
+                    rows={5}
+                    onChange={handleChange}
+                    // placeholder="Password"
+                    // onChange={(e) => setMessage(e.target.value)}
+                    // style={{height: "100px"}}
+                  ></textarea>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-center">
+                    <button
+                      type="submit"
+                      // onClick={handleSubmit}
+                      className="btn btn-md"
+                      style={{
+                        background:
+                          "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,117,255,1) 50%, rgba(0,0,0,1) 100%)",
+                        color: "white",
+                        width: "60%",
+                      }}
+                    >
+                      Send
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Support;
