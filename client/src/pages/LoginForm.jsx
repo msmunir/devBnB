d7b3be7652dc6edd9f0fbd93c09b84f81534d75c
Shahriar Munir <smunir07@gmail.com>
2023-11-30 11:09:18 +0100

    log-reg v1.5

--- client/src/pages/LoginForm.jsx
+++ client/src/pages/LoginForm.jsx
@@ -1,7 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import { useUser } from "../context/UseContext";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const user = useUser();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // function for handling changes to form inputs
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    if (id === "email") {
+      setEmail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+
+  // function for handling form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/users/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log(response);
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        user.setToken("token", response.data.token);
+        navigate("/");
+      } else {
+        alert("Error logging in user");
+        console.log(response);
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Error logging in user");
+    }
+  };
+
+  return (
+    <div className="my-5" style={{ background: "black", borderRadius: "1rem" }}>
+      <section className=" dark-grey-text mb-5">
+        <div
+          className="card"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(108,108,108,1) 50%, rgba(0,0,0,1) 100%)",
+            borderRadius: "1rem",
+            color: "white",
+          }}
+        >
+          <div className="card-body p-5">
+            <h3 className=" text-center font-weight-bold my-4">Login</h3>
+            <form className="mb-4 mx-md-5" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    value={email}
+                    onChange={handleChange}
+                    // placeholder="Email"
+                    // onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    value={password}
+                    onChange={handleChange}
+                    // placeholder="Password"
+                    // onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-center">
+                    <button
+                      type="submit"
+                      // onClick={handleSubmit}
+                      className="btn btn-md"
+                      style={{
+                        background:
+                          "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,117,255,1) 50%, rgba(0,0,0,1) 100%)",
+                        color: "white",
+                        width: "60%",
+                      }}
+                    >
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default LoginForm;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,142 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useUser } from "../context/UseContext";
 
 const Register = () => {
-  return <div>Register</div>;
+  const user = useUser();
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  // function for handling changes to form inputs
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [id]: value,
+    }));
+  };
+
+  // function for handling form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/users/register",
+        formData
+      );
+      console.log(response);
+
+      // set user context
+      if (response.data.token) {
+        localStorage.setItem("token", response.data.token);
+        user.setToken("token", response.data.token);
+        navigate("/login");
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Error registering user");
+    }
+  };
+
+  return (
+    <div className="my-5" style={{ background: "black", borderRadius: "1rem" }}>
+      <section className=" dark-grey-text mb-5">
+        <div
+          className="card"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(108,108,108,1) 50%, rgba(0,0,0,1) 100%)",
+            borderRadius: "1rem",
+            color: "white",
+          }}
+        >
+          <div className="card-body p-5">
+            <h3 className=" text-center font-weight-bold my-4">Register</h3>
+            <form className="mb-4 mx-md-5" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    className="form-control"
+                    value={formData.firstName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    className="form-control"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    className="form-control"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12 mb-4">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control"
+                    value={formData.password}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="text-center">
+                    {/* <button type="submit" className="btn btn-info btn-md"> */}
+                    <button
+                      type="submit"
+                      // onClick={handleSubmit}
+                      className="btn btn-md"
+                      style={{
+                        background:
+                          "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,117,255,1) 50%, rgba(0,0,0,1) 100%)",
+                        color: "white",
+                        width: "60%",
+                      }}
+                    >
+                      Register
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Register;
